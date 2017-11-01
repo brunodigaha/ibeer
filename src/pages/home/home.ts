e05939b08a641639ddde36c64724fa4577648ac5
Bruno Digaha <brunodigaha@gmail.com>
2017-11-01 11:38:59 -0200

    Ajustando codigo firebase em home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,25 +2,26 @@ import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { CompaniesService } from '../../services/companies.service';
 import { DetailPage } from '../detail/detail';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  companies = [];
+  companies: Observable<any[]>;
   length = 0;
   @ViewChild('myNav') nav: NavController
-  constructor(public navCtrl: NavController, public companiesService : CompaniesService) {
-    // this.companies = companiesService.getCompanies();
-    companiesService.getCompanies().subscribe( companies => {
-      this.companies = Object.assign(companies);
-      this.length = Object.keys(companies).length;
-    });
+  constructor(public navCtrl: NavController, public companiesService: CompaniesService) {
+    this.companies = companiesService.getCompanies();
+    // companiesService.getCompanies().subscribe( companies => {
+    //   this.companies = Object.assign(companies);
+    //   this.length = Object.keys(companies).length;
+    // });
   }
 
-  public goToDetail(id){
-    this.navCtrl.push(DetailPage, {id:id});
+  public goToDetail(id) {
+    this.navCtrl.push(DetailPage, { id: id });
   }
 
 
