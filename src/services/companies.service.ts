9c5c7f27106c3fb4fff6e0230df03257f949db33
Mauro Martins <mmartins@MauroJrs-MacBook-Pro.local>
2017-10-31 22:39:41 -0200

    ajustando bairro do empório da cerveja

--- src/services/companies.service.ts
+++ src/services/companies.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 export class CompaniesService {
     companies = [
         { id: 1, title: "Ponto da Cerveja", nickname: "pontodacerveja", address: "R. Salesianos, 425", neighborhood: "Pq. das Orquídeas", city: "Guaxupé", state: "MG", zipcode: "37800-000", phone: "35 3551 7001" },
-        { id: 2, title: "Emporio da Cerveja", nickname: "emporiodacerveja", address: "R. São João, 45", neighborhood: "Pq. Municípios", city: "Guaxupé", state: "MG", zipcode: "37800-000", phone: "35 3551 1145" },
+        { id: 2, title: "Emporio da Cerveja", nickname: "emporiodacerveja", address: "R. São João, 45", neighborhood: "Centro", city: "Guaxupé", state: "MG", zipcode: "37800-000", phone: "35 3551 1145" },
         { id: 3, title: "Cervejão", nickname: "cervejao", address: "Av. Dona Floriana, 948", neighborhood: "Centro", city: "Guaxupé", state: "MG", zipcode: "37800-000", phone: "35 3551 8801" },
         { id: 4, title: "Disk Cerveja do Broa", nickname: "diskcervejadobroa", address: "R. Manoel Cabral, 300", neighborhood: "Vila Carloni", city: "Guaxupé", state: "MG", zipcode: "37800-000", phone: "35 3551 5007" },
         { id: 5, title: "Tonin Atacado", nickname: "toninatacado", address: "R. Cap. Martimiano Prado, 95", neighborhood: "Centro", city: "Guaxupé", state: "MG", zipcode: "37800-000", phone: "35 3559 2550"},
