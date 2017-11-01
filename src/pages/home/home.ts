f69019e0cd6c10c8431791abb37c9ad7856bc816
Mauro Martins <mmartins@MauroJrs-MacBook-Pro.local>
2017-11-01 09:17:05 -0200

    quantidade de empresas dinamico baseado nos dados do firebase

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,16 +9,19 @@ import { DetailPage } from '../detail/detail';
 })
 export class HomePage {
   companies = [];
+  length = 0;
   @ViewChild('myNav') nav: NavController
   constructor(public navCtrl: NavController, public companiesService : CompaniesService) {
     // this.companies = companiesService.getCompanies();
     companiesService.getCompanies().subscribe( companies => {
       this.companies = Object.assign(companies);
+      this.length = Object.keys(companies).length;
     });
   }
 
   public goToDetail(id){
     this.navCtrl.push(DetailPage, {id:id});
   }
+
 
 }
--- www/build/main.js
+++ www/build/main.js
@@ -43,10 +43,9 @@ var CompaniesService = (function () {
 }());
 CompaniesService = __decorate([
     Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["B" /* Injectable */])(),
-    __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1_angularfire2_database_database__["a" /* AngularFireDatabase */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_angularfire2_database_database__["a" /* AngularFireDatabase */]) === "function" && _a || Object])
+    __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_angularfire2_database_database__["a" /* AngularFireDatabase */]])
 ], CompaniesService);
 
-var _a;
 //# sourceMappingURL=companies.service.js.map
 
 /***/ }),
@@ -89,17 +88,7 @@ var DetailPage = (function () {
         this.companiesService = companiesService;
         this.modalCtrl = modalCtrl;
         this.callNumber = callNumber;
-        this.company = {
-            id: null,
-            title: null,
-            nickname: null,
-            address: null,
-            neighborhood: null,
-            city: null,
-            state: null,
-            zipcode: null,
-            phone: null
-        };
+        this.company = { id: null, title: null };
         this.id = null;
         this.id = navParams.get('id');
         if (this.id != 0) {
@@ -133,10 +122,9 @@ DetailPage = __decorate([
     Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
         selector: 'page-detail',template:/*ion-inline-start:"/Users/mmartins/Documents/bitbucket/appcerveja/src/pages/detail/detail.html"*/'<!--\n  Generated template for the DetailPage page.\n\n  See http://ionicframework.com/docs/components/#navigation for more info on\n  Ionic pages and navigation.\n-->\n<ion-header>\n\n  <ion-navbar>\n    <ion-title>Empresa</ion-title>\n  </ion-navbar>\n\n</ion-header>\n\n<ion-content padding>\n  <img src="../../assets/imgs/large-{{company.nickname}}.png">\n  <ion-item>\n    <h1>{{ company.title }}</h1>\n    <p><ion-icon name="call"> {{ company.phone }} </ion-icon></p>\n    <p><ion-icon name="home"> {{ company.address }} - {{ company.city }}/{{ company.state }}</ion-icon></p>\n  </ion-item>\n  <ion-row>\n      <ion-col>\n        <button ion-button icon-left clear small>\n          <ion-icon name="thumbs-up"></ion-icon>\n          <div>12 Curtidas</div>\n        </button>\n      </ion-col>\n      <ion-col>\n        <button ion-button icon-left clear small (click)="openModal()">\n          <ion-icon name="text"></ion-icon>\n          <div>4 Comentários</div>\n        </button>\n      </ion-col>\n    </ion-row>\n  <ion-card-content>\n    <p>O Ponto da Cerveja é um local para compra de bebidas em geral, funcionando a mais de 5 anos em Guaxupé. Como uma distribuidora\n      de bebidas realiza entrega para a cidade. Ligue e compre !\n    </p>\n  </ion-card-content>\n</ion-content>\n\n<ion-footer>\n  <ion-toolbar>\n      <ion-title (click)="callPhone(company.phone)"><button ion-button color="secondary" full><ion-icon name="call"></ion-icon><span margin-left>Chamar</span></button></ion-title>\n  </ion-toolbar>\n</ion-footer>'/*ion-inline-end:"/Users/mmartins/Documents/bitbucket/appcerveja/src/pages/detail/detail.html"*/
     }),
-    __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* NavController */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* NavController */]) === "function" && _a || Object, typeof (_b = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["g" /* NavParams */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["g" /* NavParams */]) === "function" && _b || Object, typeof (_c = typeof __WEBPACK_IMPORTED_MODULE_2__services_companies_service__["a" /* CompaniesService */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_2__services_companies_service__["a" /* CompaniesService */]) === "function" && _c || Object, typeof (_d = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["e" /* ModalController */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["e" /* ModalController */]) === "function" && _d || Object, typeof (_e = typeof __WEBPACK_IMPORTED_MODULE_4__ionic_native_call_number__["a" /* CallNumber */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_4__ionic_native_call_number__["a" /* CallNumber */]) === "function" && _e || Object])
+    __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* NavController */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["g" /* NavParams */], __WEBPACK_IMPORTED_MODULE_2__services_companies_service__["a" /* CompaniesService */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["e" /* ModalController */], __WEBPACK_IMPORTED_MODULE_4__ionic_native_call_number__["a" /* CallNumber */]])
 ], DetailPage);
 
-var _a, _b, _c, _d, _e;
 //# sourceMappingURL=detail.js.map
 
 /***/ }),
@@ -268,9 +256,11 @@ var HomePage = (function () {
         this.navCtrl = navCtrl;
         this.companiesService = companiesService;
         this.companies = [];
+        this.length = 0;
         // this.companies = companiesService.getCompanies();
         companiesService.getCompanies().subscribe(function (companies) {
             _this.companies = Object.assign(companies);
+            _this.length = Object.keys(companies).length;
         });
     }
     HomePage.prototype.goToDetail = function (id) {
@@ -284,7 +274,7 @@ __decorate([
 ], HomePage.prototype, "nav", void 0);
 HomePage = __decorate([
     Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
-        selector: 'page-home',template:/*ion-inline-start:"/Users/mmartins/Documents/bitbucket/appcerveja/src/pages/home/home.html"*/'<ion-header>\n  <ion-navbar>\n    <ion-title>\n      App Cerveja\n    </ion-title>\n  </ion-navbar>\n</ion-header>\n\n<ion-content padding>\n  <ion-list>\n\n\n    <ion-card *ngFor="let company of companies" (click)="goToDetail(company.id)">\n      <!-- {{(companies | async)?.length}} Empresas -->\n      <ion-item>\n        <ion-avatar item-start>\n          <img src="../../assets/imgs/thumb-{{company.nickname}}.png">\n        </ion-avatar>\n        <h2>{{ company.title }}</h2>\n        <p> {{ company.phone}}</p>\n      </ion-item>\n\n      <img src="img/advance-card-bttf.png">\n\n      <ion-card-content>\n        <p>Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine... out of a DeLorean?!\n          Whoa. This is heavy.</p>\n      </ion-card-content>\n\n      <ion-row>\n        <ion-col>\n          <button ion-button icon-left clear small>\n            <ion-icon name="thumbs-up"></ion-icon>\n            <div>12</div>\n          </button>\n        </ion-col>\n        <ion-col>\n          <button ion-button icon-left clear small>\n            <ion-icon name="text"></ion-icon>\n            <div>4</div>\n          </button>\n        </ion-col>\n        <ion-col center text-center>\n          <ion-note>\n            11h ago\n          </ion-note>\n        </ion-col>\n      </ion-row>\n\n    </ion-card>\n\n  </ion-list>\n</ion-content>'/*ion-inline-end:"/Users/mmartins/Documents/bitbucket/appcerveja/src/pages/home/home.html"*/
+        selector: 'page-home',template:/*ion-inline-start:"/Users/mmartins/Documents/bitbucket/appcerveja/src/pages/home/home.html"*/'<ion-header>\n  <ion-navbar>\n    <ion-title>\n      App Cerveja\n    </ion-title>\n  </ion-navbar>\n</ion-header>\n\n<ion-content padding>\n  <ion-list>\n    <span>{{ length }} Empresas</span>\n    <ion-card *ngFor="let company of companies" (click)="goToDetail(company.id)">\n      <ion-item>\n        <ion-avatar item-start>\n          <img src="../../assets/imgs/thumb-{{company.nickname}}.png">\n        </ion-avatar>\n        <h2>{{ company.title }}</h2>\n        <p> {{ company.phone}}</p>\n      </ion-item>\n\n      <img src="img/advance-card-bttf.png">\n\n      <ion-card-content>\n        <p>Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine... out of a DeLorean?!\n          Whoa. This is heavy.</p>\n      </ion-card-content>\n\n      <ion-row>\n        <ion-col>\n          <button ion-button icon-left clear small>\n            <ion-icon name="thumbs-up"></ion-icon>\n            <div>12</div>\n          </button>\n        </ion-col>\n        <ion-col>\n          <button ion-button icon-left clear small>\n            <ion-icon name="text"></ion-icon>\n            <div>4</div>\n          </button>\n        </ion-col>\n        <ion-col center text-center>\n          <ion-note>\n            11h ago\n          </ion-note>\n        </ion-col>\n      </ion-row>\n\n    </ion-card>\n\n  </ion-list>\n</ion-content>'/*ion-inline-end:"/Users/mmartins/Documents/bitbucket/appcerveja/src/pages/home/home.html"*/
     }),
     __metadata("design:paramtypes", [typeof (_b = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* NavController */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* NavController */]) === "function" && _b || Object, typeof (_c = typeof __WEBPACK_IMPORTED_MODULE_2__services_companies_service__["a" /* CompaniesService */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_2__services_companies_service__["a" /* CompaniesService */]) === "function" && _c || Object])
 ], HomePage);
