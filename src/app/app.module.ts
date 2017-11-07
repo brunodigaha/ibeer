8610a17a9b440be67186175a7c1eb2a911049101
Bruno Digaha <brunodigaha@gmail.com>
2017-11-07 11:57:13 -0200

    Insereindo dependencias na configuração do projeto ionic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { CallNumber } from '@ionic-native/call-number'
+import { Facebook } from '@ionic-native/facebook'
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -54,7 +55,8 @@ export const firebaseConfig = {
     { provide: ErrorHandler, useClass: IonicErrorHandler },
     CompaniesService,
     AuthService,
-    CallNumber
+    CallNumber,
+    Facebook
   ]
 })
 export class AppModule { }
